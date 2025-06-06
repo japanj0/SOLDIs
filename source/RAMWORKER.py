7c7ad3b3f4889abb1a1c30816fdb6957db889646
japanj0 <socolov9066@gmail.com>
2025-06-06 20:16:14 +0300

    доработал проблему с оперативой за счет ещё одного модуля

---
+++ source/RAMWORKER.py
@@ -0,0 +1,10 @@
+import psutil
+def clearing_RAM():
+    drivers = ['geckodriver.exe', 'chromedriver.exe', 'msedgedriver.exe']
+
+    for proc in psutil.process_iter(['name']):
+        if proc.info['name'] in drivers:
+            try:
+                proc.terminate()
+            except Exception as e:
+                print(e)
